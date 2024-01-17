05c5a2edd603470745a987510dea64714353e50a
Scott Hahn <scotthahn2210@gmail.com>
2024-01-17 18:12:48 +0200

    removed uncommented code

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -39,39 +39,6 @@ usersRouter.get('/:username', async (req, res) => {
     }) 
 })
 
-// get single user
-// usersRouter.get('/:id' async (req, res) => {
-//     const { id } = req.params;
-//     pool.getConnection((err: any, connection: any) => {
-//         if (err) {
-//             console.log(err)
-//             res.send({
-//                 success: false,
-//                 statusCode: 500,
-//                 message: 'Error encountered during connection'
-//             })
-//             return;
-//         }
-//         connection.query('SELECT * FROM users WHERE id=?', [id], (err: any, rows: any) => {
-//             if (err) {
-//                 connection.release();
-//                 return res.send({
-//                     success: false,
-//                     statusCode: 400
-//                 })
-//             }
-
-//             res.send({
-//                 message: 'Success',
-//                 statusCode: 200,
-//                 data: rows
-//             })
-
-//             connection.release();
-//         })
-//     }) 
-// })
-
 //register 
 usersRouter.post('/register', (req, res) => {
     const sql = 'INSERT INTO users (username, password) VALUES (?, ?)';
@@ -85,33 +52,11 @@ usersRouter.post('/register', (req, res) => {
                 if (err) {
                     return res.json({ Message: 'Error signing up' })
                 }
-                return res.json(result);
+                return res.json({ registered: true, result });
             })
         }
     })
 })
-
-//check auth middleware
-// const verifyJwt = (req: any, res: any, next: any) => {
-//     const token = req.headers["access-token"];
-//     if (!token) {
-//         return res.json('No token found');
-//     } else {
-//         jwt.verify(token, 'jwtSecretKey', (err: any, decoded: any) => {
-//             if (err) {
-//                 res.json('Not authenticated');
-//             } else {
-//                 req.userId = decoded.id;
-//                 next();
-//             }
-//         })
-//     }
-// }
-
-// // check auth
-// usersRouter.get('/checkAuth', verifyJwt, (req, res) => {
-//     return res.json('Authenticated');
-// })
 
 //login
 usersRouter.post('/login', (req, res) => {
@@ -141,126 +86,5 @@ usersRouter.post('/login', (req, res) => {
 
     })
 })
-
-
-
-//register user
-// usersRouter.post('/register', async (req, res) => {
-//     const { username, password } = req.body;
-
-//     if (username && password) {
-//         pool.getConnection((err: any, connection: any) => {
-//             if (err) {
-//                 console.log(err);
-
-//                 res.send({
-//                     success: false,
-//                     statusCode: 500,
-//                     message: 'Error encountered during connection'
-//                 })
-
-//                 return;
-//             }
-
-//             bcrypt.hash(password, salt, async (error: any, hash: string) => {
-//                 if (error) {
-//                     res.send({
-//                         success: false,
-//                         statusCode: 500,
-//                         message: "Error found during connection",
-//                     });
-
-//                     return;
-//                 } else {
-//                     connection.query('INSERT INTO users (username, password) VALUES (?, ?)', [username, hash], (err: any, rows: any) => {
-//                         if (err.code === 'ER_DUP_ENTRY') {
-//                             connection.release();
-//                             console.log(err);
-//                             return res.send({
-//                                 success: false,
-//                                 statusCode: 400,
-//                                 message: err.code
-//                             });
-//                         } else if (err) {
-//                             connection.release();
-//                             console.log(err);
-//                             return res.send({
-//                                 success: false,
-//                                 statusCode: 400,
-//                             });
-//                         }
-                        
-//                         console.log(req.body);
-//                         res.send({
-//                             message: 'Sucess',
-//                             statusCode: 200,
-//                             // data: rows
-//                         });
-
-//                         connection.release();
-//                     })
-//                 }
-//             })
-//         })
-//     }
-// })
-
-//user login
-// usersRouter.post('/login', (req, res) => {
-//     pool.getConnection((err: any, connection: any) => {
-//         if (err) {
-//             console.log(err);
-//             res.send({
-//                 success: false,
-//                 statusCode: 500,
-//                 message: 'Error encountered during connection'
-//             })
-//             return;
-//         }
-//         // console.log(req.body.username)
-//         pool.query('SELECT password FROM users WHERE username=?', [req.body.username], (err: any, rows: any) => {
-//             if (err) {
-//                 connection.release();
-//                 res.send({
-//                     success: false,
-//                     statusCode: 400,
-//                     data: err
-//                 });
-//                 return;
-//             }
-
-//             // console.log(rows)
-
-//             const hash = rows[0].password;
-
-//             // Load hash from your password DB.
-//             bcrypt.compare(req.body.password, hash, function(err, result) {
-//                 if (err) {
-//                     res.send({
-//                         message: 'failed',
-//                         statusCode: 500,
-//                         data: err
-//                     });
-//                 }
-
-//                 if (result) {
-//                     res.send({
-//                         message: 'Success',
-//                         statusCode: 200,
-//                         data: {token: generateToken(req.body.username)}
-//                     });
-//                 } else {
-//                     res.send({
-//                         message: 'failed',
-//                         statusCode: 500,
-//                         data: err
-//                     });
-//                 }
-//             });
-            
-//             connection.release();
-//         })
-//     })
-// })
 
 export default usersRouter;
