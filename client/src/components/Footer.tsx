54305189433679790a6e4751fb96ee8684a9e370
Scott Hahn <scotthahn2210@gmail.com>
2024-01-18 02:43:39 +0200

    added links

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,8 +1,8 @@
 import '../styles/Footer.css';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagram, faFacebook, faTwitter, faLinkedin, faGithub, } from '@fortawesome/free-brands-svg-icons';
-import { faHome, faPeopleGroup, faBookOpen, faFilm, faTv } from '@fortawesome/free-solid-svg-icons';
+import {  faLinkedin, faGithub, } from '@fortawesome/free-brands-svg-icons';
+import { faHome, faPeopleGroup, faFilm, faTv } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
   return (
@@ -13,27 +13,21 @@ const Footer = () => {
             <li>Home &nbsp; <FontAwesomeIcon icon={faHome} /></li>
           </Link>
 
-          <Link to="/characters">
+          <Link to="/popular/people">
             <li>People &nbsp; <FontAwesomeIcon icon={faPeopleGroup} /></li>
           </Link>
 
-          <Link to="/movies">
+          <Link to="/popular/movies">
             <li>Movies &nbsp; <FontAwesomeIcon icon={faFilm} /></li>
           </Link>
 
-          <Link to="/series">
+          <Link to="/popular/tv">
             <li>TV Shows <FontAwesomeIcon icon={faTv} /></li>
           </Link>
         </ul>
 
         <ul className="social-media-links">
-          <a href="https://www.instagram.com/marvel" target="_blank">
-            <li>Instagram &nbsp; <FontAwesomeIcon icon={faInstagram} /></li>
-          </a>
-          <a href="https://twitter.com/marvel" target="_blank">
-            <li>Twitter &nbsp; <FontAwesomeIcon icon={faTwitter} /></li>
-          </a>
-          <a href="https://www.facebook.com/Marvel" target="_blank" >
+          <a href="https://www.linkedin.com/in/scott-hahn" target="_blank" >
             <li>LinkedIn &nbsp; <FontAwesomeIcon icon={faLinkedin} /></li>
           </a>
           <a href='https://github.com/ScottHahn1' target='_blank'>
