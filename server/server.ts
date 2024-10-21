78d093419b209796a8d99c7a6cec6418f96c0e5f
ScottHahn1 <scotthahn2210@gmail.com>
2024-10-21 14:33:39 +0200

    added alternative port number

--- server/server.ts
+++ server/server.ts
@@ -12,7 +12,7 @@ import tvRouter from "./routes/tv";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3306;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
