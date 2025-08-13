75282fc49f39381993a5795523cbb9e4c2a38228
ScottHahn1 <scotthahn2210@gmail.com>
2025-08-13 20:42:18 +0200

    refactor: rewrite Search component to use URL params - Simplified state and pagination for movies, shows, and people

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -1,14 +1,16 @@
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import useAxios from "./useAxios";
 import '../styles/Search.css';
-import { Clicked } from "../App";
+import SearchResults from "./SearchResults";
+import { useParams } from "react-router-dom";
+import MorePages from "./MorePages";
 
 type Params = { 
-    query: string,
+    query?: string,
     page: number
- }
+}
 
-type InitialData = {
+type Data = {
     results: {
         backdrop_path: string,
         genre_ids: [],
@@ -28,74 +30,39 @@ type InitialData = {
     total_results: number
 }
 
-type Data = {
-    backdrop_path: string,
-    genre_ids: [],
-    id: number,
-    media_type: string,
-    overview: string,
-    poster_path: string,
-    profile_path: string,
-    release_date: string,
-    first_air_date: string,
-    name: string,
-    title: string,
-    vote_average: number,
-    known_for_department: string
-}[]
-
-const Search = ({ clicked, setClicked, searched }: { clicked: Clicked, setClicked: Dispatch<SetStateAction<Clicked>>, searched: string }) => {
-    const [movies, setMovies] = useState<Data>({} as Data);
-    const [shows, setShows] = useState<Data>({} as Data);
-    const [people, setPeople] = useState<Data>({} as Data);
+const Search = () => {
+    const { query } = useParams();
     const [pageNum, setPageNum] = useState(1);
     const [totalPages, setTotalPages] = useState(0);
 
-    const noImgFound = require('../assets/images/no-image-found.jpg');
+    const [searchResultsType, setSearchResultsType] = useState('movies');
 
     const params = { 
-        query: searched,
-        page: pageNum
+        query,
+        page: pageNum,
+        limit: 20
     };
 
-    const { data: moviesData } = useAxios<InitialData, Params>(`https://movie-db-omega-ten.vercel.app/search/movies`, {} as InitialData, params, pageNum);
-    const { data: showsData } = useAxios<InitialData, Params>(`https://movie-db-omega-ten.vercel.app/search/shows`, {} as InitialData, params, pageNum);
-    const { data: peopleData } = useAxios<InitialData, Params>(`https://movie-db-omega-ten.vercel.app/search/people`, {} as InitialData, params, pageNum);
-    
-    useEffect(() => {
-        Object.keys(moviesData).length && setMovies(moviesData.results);
-    }, [moviesData])
+    const { data: movies } = useAxios<Data, Params>(`https://movie-db-omega-ten.vercel.app/search/movies`, {} as Data, params, pageNum);
+    const { data: shows } = useAxios<Data, Params>(`https://movie-db-omega-ten.vercel.app/search/shows`, {} as Data, params, pageNum);
+    const { data: people } = useAxios<Data, Params>(`https://movie-db-omega-ten.vercel.app/search/people`, {} as Data, params, pageNum);
 
     useEffect(() => {
-        Object.keys(showsData).length && setShows(showsData.results);
-    }, [showsData])
-
-    useEffect(() => {
-        Object.keys(peopleData).length && setPeople(peopleData.results);
-    }, [peopleData])
-
-    useEffect(() => {
-        if (moviesData && showsData && peopleData) {
-            if (clicked.type === 'movie') {
-                setTotalPages(moviesData.total_pages);
-                moviesData.total_pages < 500 && setTotalPages(moviesData.total_pages);
-            } else if (clicked.type === 'tv') {
-                setTotalPages(showsData.total_pages);
-                showsData.total_pages < 500 && setTotalPages(showsData.total_pages);
-            } else {
-                setTotalPages(peopleData.total_pages);
-                showsData.total_pages < 500 && setTotalPages(peopleData.total_pages);
+        if (movies && shows && people) {
+            if (searchResultsType === 'movies') {
+                setTotalPages(movies.total_pages);
+            } else if (searchResultsType === 'shows') {
+                setTotalPages(shows.total_pages);
+            } else if (searchResultsType === 'people') {
+                setTotalPages(people.total_pages);
             }
         }
-    }, [moviesData, showsData, peopleData, clicked.type])
+    }, [movies, shows, people, pageNum, searchResultsType])
 
-    useEffect(() => {
+    const handleSearchTypeChange = (type: string) => {
+        setSearchResultsType(type);
         setPageNum(1);
-    }, [clicked.type])
-    
-    useEffect(() => {
-        totalPages && totalPages > 500 && setTotalPages(500);
-    }, [totalPages])
+    }
 
     return (
         <div className='search-container'>
@@ -103,87 +70,98 @@ const Search = ({ clicked, setClicked, searched }: { clicked: Clicked, setClicke
                 <div className='search-results-heading' style={{ backgroundColor: 'lightgreen', width: '100%'}}>
                     <h4>Search Results</h4>
                 </div>
-                <div className={ clicked.type === 'movie' ? 'search-results-type active' : 'search-results-type' } onClick={() => setClicked({ type: 'movie' })}>
-                    <p>Movies</p>
-                    <span style={{ fontWeight: 'normal' }}>({ moviesData.total_results })</span>
+
+                <div 
+                    className={ searchResultsType === 'movies' ? 'search-results-type active' : 'search-results-type' } 
+                    onClick={() => handleSearchTypeChange('movies')}
+                >
+                    <span>Movies</span>
+                    <span style={{ fontWeight: 'normal' }}>({ movies.total_results })</span>
                 </div>
-                <div className={ clicked.type === 'show' ? 'search-results-type active' : 'search-results-type' } onClick={() => setClicked({ type: 'tv' })}>
-                    <p>TV Shows</p>
-                    <span style={{ fontWeight: 'normal' }}>({ showsData.total_results })</span>
+                <div 
+                    className={ searchResultsType === 'shows' ? 'search-results-type active' : 'search-results-type' } 
+                    onClick={() => handleSearchTypeChange('shows')}
+                >
+                    <span>TV Shows</span>
+                    <span style={{ fontWeight: 'normal' }}>({ shows.total_results })</span>
                 </div>
-                <div className={ clicked.type === 'person' ? 'search-results-type active' : 'search-results-type' } onClick={() => setClicked({ type: 'person' })}>
-                    <p>People</p>
-                    <span style={{ fontWeight: 'normal' }}>({ peopleData.total_results })</span>
+                <div 
+                    className={ searchResultsType === 'people' ? 'search-results-type active' : 'search-results-type' } 
+                    onClick={() => handleSearchTypeChange('people')}
+                >
+                    <span>People</span>
+                    <span style={{ fontWeight: 'normal' }}>({ people.total_results })</span>
                 </div>
             </div>
 
             <div className='search-data'>
                 {
-                    movies.length > 0 && clicked.type === 'movie' && movies.map(movie => (
-                        <div className='search-movie'>
-                            <div>
-                                <img src={ movie.poster_path ? `https://image.tmdb.org/t/p/w300/${movie.poster_path}` : noImgFound } alt={movie.title} />
-                            </div>
-                            <div>
-                                <h4>{movie.title}</h4>
-                                <p>{ new Date(movie.release_date).toString().slice(4, 15) }</p>
-                                <p>{movie.overview}</p>
-                            </div>
-                        </div>
+                    movies?.results?.length > 0 &&
+                    searchResultsType === 'movies' && 
+                    movies.results.map(movie => (
+                        <SearchResults 
+                            key={movie.id}
+                            image={movie.poster_path} 
+                            name={movie.title} 
+                            overview={movie.overview}
+                            releaseDate={movie.release_date} 
+                            searchResultsType={searchResultsType} 
+                        />
                     ))
                 }
+
                 {
-                    shows.length > 0 && clicked.type === 'tv' && shows.map(show => (
-                        <div className='search-movie'>
-                            <div>
-                                <img src={ show.poster_path ? `https://image.tmdb.org/t/p/w300/${show.poster_path}` : noImgFound } alt={show.name} />
-                            </div>
-                            <div>
-                                <h4>{show.name}</h4>
-                                <p>{new Date(show.first_air_date).toString().slice(4, 15)}</p>
-                                <p>{show.overview}</p>
-                            </div>
-                        </div>
+                    shows?.results?.length > 0 &&
+                    searchResultsType === 'shows' && 
+                    shows.results.map(show => (
+                        <SearchResults 
+                            key={show.id}
+                            image={show.poster_path} 
+                            name={show.name} 
+                            overview={show.overview} 
+                            releaseDate={show.first_air_date}
+                            searchResultsType={searchResultsType} 
+                        />
                     ))
                 }
+
                 {
-                    people.length > 0 && clicked.type === 'person' && people.map(person => (
-                        <div className='search-movie'>
-                            <div>
-                                <img src={ person.poster_path ? `https://image.tmdb.org/t/p/w300/${person.profile_path}` : noImgFound } alt={person.name} />
-                            </div>
-                            <div>
-                                <h4>{person.name}</h4>
-                                <p>{person.known_for_department}</p>
-                            </div>
-                        </div>
+                    people?.results?.length > 0 &&
+                    searchResultsType === 'people' && 
+                    people.results.map(person => (
+                        <SearchResults 
+                            key={person.id}
+                            actingCredits={person.known_for_department}
+                            image={person.poster_path} 
+                            name={person.name} 
+                            searchResultsType={searchResultsType} 
+                        />
                     ))
                 }
             </div>
 
-            <div className='next'>
-                {
-                    pageNum > 2 && (
-                        <button onClick={() => setPageNum(prev => prev - 2)}>{ pageNum - 2 }</button>
-                    )
-                }
+            <div className='pages'>
 
-                {
-                    pageNum > 1 && (
-                        <button onClick={() => setPageNum(prev => prev - 1)}>{ pageNum - 1 }</button>
-                    )
-                }
+            {
+                pageNum !== 1 &&
+                <button onClick={() => setPageNum(page => Math.max(page - 1, 1))}>
+                    Previous
+                </button>
+            }
 
-                { pageNum < totalPages && <button>{ pageNum }</button> }
+            <MorePages pageNum={pageNum} setPageNum={setPageNum} totalPages={totalPages} />
 
-                { pageNum + 1 < totalPages && <button onClick={() => setPageNum(prev => prev + 1)}>{ pageNum + 1 }</button> }
-                { pageNum + 2 < totalPages && <button onClick={() => setPageNum(prev => prev + 2)}>{ pageNum + 2 }</button> }
-                { pageNum + 3 < totalPages && <button onClick={() => setPageNum(prev => prev + 3)}>{ pageNum + 3 }</button> }
-                { pageNum + 4 < totalPages && <button onClick={() => setPageNum(prev => prev + 4)}>{ pageNum + 4 }</button> }
-                ...
-                <button onClick={() => setPageNum(totalPages)}>{ totalPages }</button> 
-                { pageNum < totalPages && <button onClick={() => setPageNum(prev => prev + 1)}>Next</button> }
-                <h4>{pageNum}</h4>
+            {
+                totalPages && pageNum !== totalPages &&
+                <button 
+                    onClick={
+                        () => setPageNum(page => (totalPages ? Math.min(page + 1, totalPages) : page + 1))
+                    } 
+                >
+                    Next
+                </button>
+            }
+
             </div>
         </div>
     )
