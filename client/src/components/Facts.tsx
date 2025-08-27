1dd694415d49bb508edc14e2b55f033938650f50
ScottHahn1 <scotthahn2210@gmail.com>
2025-08-27 21:43:53 +0200

    refactor: refactor Facts component to use useParams instead of clicked prop

--- client/src/components/Facts.tsx
+++ client/src/components/Facts.tsx
@@ -1,35 +1,35 @@
 import useAxios from "./useAxios";
-import { Clicked } from '../App';
 import { DetailsType } from "../typeAliases/Details";
+import { useParams } from "react-router-dom";
 
-const Facts = ({ clicked }: { clicked: Clicked }) => {
-    const { data, loading } = useAxios<DetailsType, {}>(`https://movie-db-omega-ten.vercel.app/movies/details/${clicked.type}/${clicked.id}`, {} as DetailsType, {});
+const Facts = () => {
+    const { id, type } = useParams();
+
+    const { data, loading } = useAxios<DetailsType, {}>(`https://movie-db-omega-ten.vercel.app/movies/details/${type}/${id}`, {} as DetailsType, {});
 
     const noImgFound = require('../assets/images/no-image-found.jpg');
 
-    return (
-        <div>
-            {
-                !loading && (
-                    <div className='facts'>
-                        <p> <b>Status</b> <br></br> { data.status } </p>
-                        <p> <b>Original Language</b> <br></br> { data.spoken_languages[0].english_name } </p>
-                        {
-                            clicked.type === 'movie' ? (
-                                <>
-                                    <p> <b>Budget</b> <br></br> { data.budget } </p>
-                                    <p> <b>Revenue</b> <br></br> { data.revenue } </p>
-                                </>
-                            ) :
-                                <>
-                                    <p> <b>Network</b> </p>
-                                    <img src={ data.networks[0].logo_path ? `https://image.tmdb.org/t/p/w92/${data.networks[0].logo_path}` : noImgFound } alt={data.networks[0].name} />
-                                    <p> <b>Type</b> <br></br>{ data.type } </p>
-                                </>
+    if (loading) {
+        return <div className='loading' />
+    }
 
-                        }
-                    </div>
-                )
+    return (
+        <div className='facts'>
+            <p> <b>Status</b> <br></br> { data.status } </p>
+            <p> <b>Original Language</b> <br></br> { data.spoken_languages[0].english_name } </p>
+            {
+                type === 'movie' ? (
+                    <>
+                        <p> <b>Budget</b> <br></br> { data.budget } </p>
+                        <p> <b>Revenue</b> <br></br> { data.revenue } </p>
+                    </>
+                ) :
+                    <>
+                        <p> <b>Network</b> </p>
+                        <img src={ data.networks[0].logo_path ? `https://image.tmdb.org/t/p/w92/${data.networks[0].logo_path}` : noImgFound } alt={data.networks[0].name} />
+                        <p> <b>Type</b> <br></br>{ data.type } </p>
+                    </>
+
             }
         </div>
     )
