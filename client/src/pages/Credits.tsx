768344423f05e1b44a8d51b6d5fa19caa3b195fd
ScottHahn1 <scotthahn2210@gmail.com>
2025-08-27 21:45:34 +0200

    refactor: removed clicked and setClicked props in favor of useParams for id and type

--- client/src/pages/Credits.tsx
+++ client/src/pages/Credits.tsx
@@ -1,68 +1,63 @@
 import useAxios from '../components/useAxios';
 import '../styles/Details.css';
-import { Clicked } from '../App';
 import '../styles/Credits.css';
 import { CreditsType } from '../typeAliases/Credits';
-import { Dispatch, SetStateAction } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
-const Credits = ({ clicked, setClicked }: { clicked: Clicked, setClicked: Dispatch<SetStateAction<Clicked>> }) => {
-    const { data: credits, loading } = useAxios<CreditsType, {}>(`https://movie-db-omega-ten.vercel.app/movies/credits/${clicked.type}/${clicked.id}`, {} as CreditsType, {});
+const Credits = () => {
+    const { id, type } = useParams();
+
+    const { data: credits, loading } = useAxios<CreditsType, {}>(`https://movie-db-omega-ten.vercel.app/movies/credits/${type}/${id}`, {} as CreditsType, {});
 
     const noImgFound = require('../assets/images/no-image-found.jpg');
 
+    if (loading) {
+        return <div className='loading' />
+    }
+
     return (
         <div className='credits-crew'>
-            {
-                !loading ? (
-                    <>
-                        <div className='cast'>
-                            <h4>Cast ({ credits.cast.length })</h4>
-                            {
-                                credits.cast.map(person => (
-                                    <div className='cast-member' key={person.id}>
-                                        <Link to='/person'>
-                                            <img 
-                                                src={ person.profile_path ? `https://image.tmdb.org/t/p/w300/${person.profile_path}` : noImgFound} 
-                                                alt={person.name} 
-                                                onClick={() => setClicked({ type: 'person', id: person.id })}
-                                            />
-                                        </Link>
-                                        <div>
-                                            <p><b>{ person.name }</b></p>
-                                            <p>{ person.character }</p>
-                                        </div>
-                                    </div>
-                                ))
-                            }
-                        </div>
-
-                        <div className='crew'>
-                            <h4>Crew ({ credits.crew.length })</h4>
-                            {
-                                credits.crew.map(person => (
-                                    <div className='crew-member' key={person.id}>
-                                        <Link to='/person'>
-                                            <img 
-                                                src={ person.profile_path ? `https://image.tmdb.org/t/p/w300/${person.profile_path}` : noImgFound } 
-                                                alt={person.name} 
-                                                onClick={() => setClicked({ type: 'person', id: person.id })}
-                                            />
-                                        </Link>
-                                        <div>
-                                            <p><b>{ person.name }</b></p>
-                                            <p>{ person.job }</p>
-                                        </div>
-                                    </div>
-                                ))
-                            }
-                        </div>
-                    </>
-                )
-                :
-                <div className='loading'>
+            <>
+                <div className='cast'>
+                    <h4>Cast ({ credits.cast.length })</h4>
+                    {
+                        credits.cast.map(person => (
+                            <div className='cast-member' key={person.id}>
+                                <Link to={`/person/${person.id}/${person.name.replace(/\s+/g, '-')}`}>
+                                    <img 
+                                        src={ person.profile_path ? `https://image.tmdb.org/t/p/w300/${person.profile_path}` : noImgFound} 
+                                        alt={person.name} 
+                                    />
+                                </Link>
+                                <div>
+                                    <p><b>{ person.name }</b></p>
+                                    <p>{ person.character }</p>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
-            }
+
+                <div className='crew'>
+                    <h4>Crew ({ credits.crew.length })</h4>
+                    {
+                        credits.crew.map(person => (
+                            <div className='crew-member' key={`${person.id}-${person.job}`}>
+                                <Link to={`/person/${person.id}/${person.name.replace(/\s+/g, '-')}`}>
+                                    <img 
+                                        src={ person.profile_path ? `https://image.tmdb.org/t/p/w300/${person.profile_path}` : noImgFound } 
+                                        alt={person.name} 
+                                    />
+                                </Link>
+                                <div>
+                                    <p><b>{ person.name }</b></p>
+                                    <p>{ person.job }</p>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </>
         </div>
     )
 }
