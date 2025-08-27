1374da3b77bddf019ccc81c20d28bcd11fd7b113
ScottHahn1 <scotthahn2210@gmail.com>
2025-08-27 21:33:43 +0200

    refactor: remove setClicked prop and use dynamic route params for details links

--- client/src/components/Trending.tsx
+++ client/src/components/Trending.tsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
 import useAxios from './useAxios';
-import { Dispatch, SetStateAction, useState } from 'react';
-import { Clicked } from '../App';
+import { useState } from 'react';
 
 type InitialState = {
     results: {
@@ -20,61 +19,66 @@ type InitialState = {
     }[]
 }
 
-const Trending = ({ url, setClicked }: { url: string, setClicked: Dispatch<SetStateAction<Clicked>> }) => {
+const Trending = ({ url }: { url: string }) => {
     const [mediaType, setMediaType] = useState('movie');
     const { data, loading } = useAxios<InitialState, {page: number, mediaType: string}>(url, {} as InitialState, { page: 1, mediaType: mediaType }, mediaType);
 
     const noImgFound = require('../assets/images/no-image-found.jpg');
 
+    if (loading) {
+        return <div className='loading' />
+    }
+
     return (
         <div>
-            { 
-                !loading ? (
-                    <div className='data-container'>
-                        <div className='heading-buttons'>
-                            <h2>Trending</h2>
-                            <button style={{ backgroundColor: mediaType === 'movie' ? 'rgb(142, 233, 142)' : 'white' }} onClick={() => setMediaType('movie')}>Movies</button>
-                            <button style={{ backgroundColor: mediaType === 'movie' ? 'white' : 'rgb(142, 233, 142)' }} onClick={() => setMediaType('tv')}>TV Shows</button>
-                        </div>
-                        <br></br>
-                        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-                        {
-                            mediaType === 'movie' ?
-                            data.results && data.results.map((movie, index) => (
-                                <div key={ index } className='data'>
-                                    <Link to='/details'>
-                                        <img 
-                                            src={ movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : noImgFound } 
-                                            onClick={() => setClicked({ id: movie.id, type: 'movie' })}
-                                            alt={movie.title}
-                                        />
-                                    </Link>
-                                    <h5>{ movie.title }</h5>
-                                    <p>{ movie.release_date }</p>
-                                </div>
-                            ))
-                            :
-                            data.results && data.results.map((show, index) => (
-                                <div key={ index } className='data'>
-                                    <Link to='/details'>
-                                        <img 
-                                            src={ show.poster_path ? `https://image.tmdb.org/t/p/w500/${show.poster_path}` : noImgFound } 
-                                            onClick={() => setClicked({ id: show.id, type: 'tv' })}
-                                            alt={show.title}
-                                        />
-                                    </Link>
-                                    <h5>{ show.name }</h5>
-                                    <p>{ show.first_air_date }</p>
-                                </div>
-                            ))                
-                        }
-                        </div>
-                    </div>
-                )
-                :
-                <div className='loading'>
+            <div className='data-container'>
+                <div className='heading-buttons'>
+                    <h2>Trending</h2>
+
+                    <button style={{ backgroundColor: mediaType === 'movie' ? 'rgb(142, 233, 142)' : 'white' }} onClick={() => setMediaType('movie')}>
+                        Movies
+                    </button>
+
+                    <button style={{ backgroundColor: mediaType === 'movie' ? 'white' : 'rgb(142, 233, 142)' }} onClick={() => setMediaType('tv')}>
+                        TV Shows
+                    </button>
                 </div>
-            }
+
+                <br></br>
+
+                <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                    {
+                        mediaType === 'movie' ?
+                        data.results && data.results.map(movie => (
+                            <div key={movie.id} className='data'>
+                                <Link to={`/details/movie/${movie.id}/${movie.title?.replace(/\s+/g, '-')}`}>
+                                    <img 
+                                        src={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : noImgFound} 
+                                        alt={movie.title}
+                                    />
+                                </Link>
+
+                                <h5>{movie.title}</h5>
+                                <p>{movie.release_date}</p>
+                            </div>
+                        ))
+                        :
+                        data.results && data.results.map(show => (
+                            <div key={show.id} className='data'>
+                                <Link to={`/details/tv/${show.id}/${show.name?.replace(/\s+/g, '-')}`}>
+                                    <img 
+                                        src={show.poster_path ? `https://image.tmdb.org/t/p/w500/${show.poster_path}` : noImgFound} 
+                                        alt={show.title}
+                                    />
+                                </Link>
+
+                                <h5>{show.name}</h5>
+                                <p>{show.first_air_date}</p>
+                            </div>
+                        ))                
+                    }
+                </div>
+            </div>
         </div>
     )
 }
