b3ea6af1bb7f728d5b4dca63f6854d8d01bad71b
ScottHahn1 <scotthahn2210@gmail.com>
2025-09-08 13:32:35 +0200

    refactor: add id prop and wrap content in Links with dynamic routes

--- client/src/components/SearchResults.tsx
+++ client/src/components/SearchResults.tsx
@@ -1,5 +1,8 @@
+import { Link } from "react-router-dom";
+
 type Props = {
     actingCredits?: string;
+    id: number;
     image: string;
     name: string;
     releaseDate?: string;
@@ -7,25 +10,47 @@ type Props = {
     searchResultsType: string;
 };
 
-const SearchResults = ({ actingCredits, image, name, releaseDate, overview, searchResultsType }: Props) => {
+const generateLink = (searchResultsType: string, id: number, name: string) => {
+    if (searchResultsType === 'people') {
+        return `/person/${id}/${name.replace(/\s+/g, '-')}`;
+    } else {
+        return (
+            `/details/${searchResultsType === 'movies' ? 
+            `movie/${id}/${name.replace(/\s+/g, '-')}` :
+            `tv/${id}/${name.replace(/\s+/g, '-')}`}`
+        )
+    }
+}
+
+const SearchResults = ({ actingCredits, id, image, name, releaseDate, overview, searchResultsType }: Props) => {
     const noImgFound = require('../assets/images/no-image-found.jpg');
 
     return (
         <div className='search-movie'>
-            <div>
-                <img src={ image ? `https://image.tmdb.org/t/p/w300/${image}` : noImgFound } alt={name} />
-            </div>
-
-            {searchResultsType !== 'people' ?
+            <Link 
+                to={generateLink(searchResultsType, id, name)}
+            >
                 <div>
-                    <h4>{name}</h4>
+                    <img src={ image ? `https://image.tmdb.org/t/p/w300/${image}` : noImgFound } alt={name} />
+                </div>
+            </Link>
+
+            {searchResultsType === 'people' ?
+                <div>
+                    <Link to={generateLink(searchResultsType, id, name)}>
+                        <h4 style={{ color: 'black' }}>{name}</h4>
+                    </Link>
+
+                    <p>{actingCredits}</p>
+                </div>
+                :
+                <div>
+                    <Link to={generateLink(searchResultsType, id, name)}>
+                        <h4 style={{ color: 'black' }}>{name}</h4>
+                    </Link>
+
                     <p>{releaseDate && new Date(releaseDate).toString().slice(4, 15)}</p>
                     <p>{overview}</p>
-                </div>
-            :
-                <div>
-                    <h4>{name}</h4>
-                    <p>{actingCredits}</p>
                 </div>
             }
         </div>
