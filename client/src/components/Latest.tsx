049f702780637f068c28f2e7d33ef0cf01c175c9
ScottHahn1 <scotthahn2210@gmail.com>
2025-09-23 16:15:01 +0200

    feat: Add error and empty state messages for latest movies/TV shows

--- client/src/components/Latest.tsx
+++ client/src/components/Latest.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import useAxios from './useAxios';
 import { useState } from 'react';
 
-type InitialState = {
+type LatestResponse = {
     results: {
         backdrop_path: 'string',
         genre_ids: [],
@@ -24,11 +24,7 @@ type Params = {
     releaseDate: string
 }
 
-type Props = {
-    showLoading: boolean;
-}
-
-const Latest = ({ showLoading }: Props) => {
+const Latest = () => {
     const [mediaType, setMediaType] = useState('movie');
     const now = new Date();
 
@@ -41,66 +37,82 @@ const Latest = ({ showLoading }: Props) => {
     const today = new Date().toISOString().slice(0, 10);
 
     const params = {
-        mediaType: mediaType,
+        mediaType,
         releaseDate: mediaType === 'movie' ? 
         `primary_release_date.gte=${lastMonth}&primary_release_date.lte=${today}` 
         : 
         `air_date.gte=${lastMonth}&air_date.lte=${today}`
     }
 
-    const { data: latest, loading } = useAxios<InitialState, Params>(
-        'https://movie-db-omega-ten.vercel.app/movies/latest', 
-        {} as InitialState, 
+    const { data: latest, loading, error } = useAxios<LatestResponse, Params>(
+        'https://movie-db-omega-ten.vercel.app/movies/latest',  
         params
     );
 
     const noImgFound = require('../assets/images/no-image-found.jpg');
 
-    if (loading && showLoading) {
+    if (loading) {
         return <div className='loading' />
     }
 
     return (
-        <div>
-            <div className='data-container'>
-                <div className='heading-buttons'>
-                    <h2>Latest</h2>
-                    <button style={{ backgroundColor: mediaType === 'movie' ? 'rgb(142, 233, 142)' : 'white' }} onClick={() => setMediaType('movie')}>Movies</button>
-                    <button style={{ backgroundColor: mediaType === 'movie' ? 'white' : 'rgb(142, 233, 142)' }} onClick={() => setMediaType('tv')}>TV Shows</button>
-                </div>
+        <div className='data-container'>
+            <div className='heading-buttons'>
+                <h2>Latest</h2>
+                <button 
+                    style={{ backgroundColor: mediaType === 'movie' ? 'rgb(142, 233, 142)' : 'white' }} 
+                    onClick={() => setMediaType('movie')}
+                >
+                    Movies
+                </button>
 
-                <br></br>
+                <button 
+                    style={{ backgroundColor: mediaType === 'movie' ? 'white' : 'rgb(142, 233, 142)' }} 
+                    onClick={() => setMediaType('tv')}
+                >
+                    TV Shows
+                </button>
+            </div>
 
-                <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-                    {
-                        mediaType === 'movie' ?
-                        latest?.results?.map(movie => (
-                            <div key={movie.id} className='data'>
-                                <Link to={`/details/movie/${movie.id}/${movie.title?.replace(/\s+/g, '-')}`}>
-                                    <img 
-                                        src={ movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : noImgFound } 
-                                        alt={movie.title}
-                                    />
-                                </Link>
-                                <h5>{ movie.title }</h5>
-                                <p>{ movie.release_date }</p>
-                            </div>
-                        ))
-                        :
-                        latest?.results?.map(show => (
-                            <div key={show.id} className='data'>
-                                <Link to={`/details/tv/${show.id}/${show.name?.replace(/\s+/g, '-')}`}>
-                                    <img 
-                                        src={ show.poster_path ? `https://image.tmdb.org/t/p/w500/${show.poster_path}` : noImgFound } 
-                                        alt={show.title}
-                                    />
-                                </Link>
-                                <h5>{ show.name }</h5>
-                                <p>{ show.first_air_date }</p>
-                            </div>
-                        ))                
-                    }
-                </div>
+            {
+                error ? 
+                <div className='error'>Error loading latest {mediaType === 'movie' ? 'movies' : 'TV shows'}.</div>
+                :
+                (!latest || !latest.results) && 
+                <div className='error'>No latest {mediaType === 'movie' ? 'movies' : 'TV shows'} available.</div>
+            }
+
+            <br></br>
+
+            <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                {
+                    mediaType === 'movie' ?
+                    latest?.results?.map(movie => (
+                        <div key={movie.id} className='data'>
+                            <Link to={`/details/movie/${movie.id}/${movie.title?.replace(/\s+/g, '-')}`}>
+                                <img 
+                                    src={ movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : noImgFound } 
+                                    alt={movie.title}
+                                />
+                            </Link>
+                            <h5>{ movie.title }</h5>
+                            <p>{ movie.release_date }</p>
+                        </div>
+                    ))
+                    :
+                    latest?.results?.map(show => (
+                        <div key={show.id} className='data'>
+                            <Link to={`/details/tv/${show.id}/${show.name?.replace(/\s+/g, '-')}`}>
+                                <img 
+                                    src={ show.poster_path ? `https://image.tmdb.org/t/p/w500/${show.poster_path}` : noImgFound } 
+                                    alt={show.title}
+                                />
+                            </Link>
+                            <h5>{ show.name }</h5>
+                            <p>{ show.first_air_date }</p>
+                        </div>
+                    ))                
+                }
             </div>
         </div>
     )
