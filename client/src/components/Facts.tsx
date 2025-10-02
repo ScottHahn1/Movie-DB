84884555344705071ea809d3ced53a2f70bfd179
ScottHahn1 <scotthahn2210@gmail.com>
2025-10-02 18:51:05 +0200

    fix: add optional chaining to prevent runtime errors when data is undefined

--- client/src/components/Facts.tsx
+++ client/src/components/Facts.tsx
@@ -6,9 +6,9 @@ const Facts = () => {
     const { id, type } = useParams();
 
     const { data, loading } = useAxios<DetailsType, {}>(
-`https://movie-db-omega-ten.vercel.app/movies/details/${type}/${id}`, 
+        `https://movie-db-omega-ten.vercel.app/movies/details/${type}/${id}`, 
         {}
-);
+    );
 
     const noImgFound = require('../assets/images/no-image-found.jpg');
 
@@ -18,19 +18,25 @@ const Facts = () => {
 
     return (
         <div className='facts'>
-            <p> <b>Status</b> <br></br> { data.status } </p>
-            <p> <b>Original Language</b> <br></br> { data.spoken_languages[0].english_name } </p>
+            <p> <b>Status</b> <br></br> { data?.status } </p>
+            <p> <b>Original Language</b> <br></br> { data?.spoken_languages[0]?.english_name } </p>
             {
                 type === 'movie' ? (
                     <>
-                        <p> <b>Budget</b> <br></br> { data.budget } </p>
-                        <p> <b>Revenue</b> <br></br> { data.revenue } </p>
+                        <p> <b>Budget</b> <br></br> { data?.budget } </p>
+                        <p> <b>Revenue</b> <br></br> { data?.revenue } </p>
                     </>
                 ) :
                     <>
                         <p> <b>Network</b> </p>
-                        <img src={ data.networks[0].logo_path ? `https://image.tmdb.org/t/p/w92/${data.networks[0].logo_path}` : noImgFound } alt={data.networks[0].name} />
-                        <p> <b>Type</b> <br></br>{ data.type } </p>
+                        <img src={ 
+                            data?.networks[0]?.logo_path ? 
+                            `https://image.tmdb.org/t/p/w92/${data.networks[0].logo_path}` 
+                            : 
+                            noImgFound 
+                        } 
+                            alt={data?.networks[0]?.name} />
+                        <p> <b>Type</b> <br></br>{ data?.type } </p>
                     </>
 
             }
