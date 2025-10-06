0c9580b6799c416868c5172a848b0a58f627e412
ScottHahn1 <scotthahn2210@gmail.com>
2025-10-06 21:27:59 +0200

    refactor: replaced separate movie/tv routes with dynamic :type parameter for Popular, NowPlaying, and TopRated components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import "./styles//App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import Trending from "./components/Trending";
 import Details from "./pages/Details";
 import { useState } from "react";
 import Person from "./pages/Person";
@@ -19,13 +18,6 @@ import NowPlaying from "./pages/NowPlaying";
 import TopRated from './pages/TopRated';
 import Upcoming from './pages/Upcoming';
 import Footer from "./components/Footer";
-import Latest from "./components/Latest";
-
-export type Clicked = {
-  id?: string | number,
-  title?: string,
-  type?: string,
-}
 
 function App() {
   const [searched, setSearched] = useState('');
@@ -34,17 +26,13 @@ function App() {
     <div className='app'>
       <BrowserRouter>
         <Navbar />
+
         <Routes>
           <Route path='/register' element={ <Register /> } />
           <Route path='/login' element={ <Login /> } />
       
-          <Route 
-            index
-            element={<Home searched={searched} setSearched={setSearched} />} 
-          />
+          <Route index element={<Home searched={searched} setSearched={setSearched} />} />
           
-          <Route path='/movies' element={ <Trending url='https://movie-db-omega-ten.vercel.app/movies/trending' /> } />
-          <Route path='/movies' element={ <Latest url='https://movie-db-omega-ten.vercel.app/movies/latest' /> } />
           <Route path='/details/:type/:id/:title' element={ <Details /> } />
           <Route path='/person/:id/:name' element={ <Person /> } /> 
           <Route path='/credits/:type/:id' element={ <Credits /> } /> 
@@ -54,19 +42,14 @@ function App() {
           { sessionStorage.getItem('userId') && <Route path='/favourites' element={ <Favourites /> } /> }
           { sessionStorage.getItem('userId') && <Route path='/ratings' element={ <Ratings /> } /> }
 
-          <Route path='/popular/movies' element={ <Popular url='https://movie-db-omega-ten.vercel.app/movies/popular' mediaType='movie' /> } />
-          <Route path='/popular/tv' element={ <Popular url='https://movie-db-omega-ten.vercel.app/tv/popular' mediaType='tv' /> } />
-
-          <Route path='/nowPlaying/movies' element={ <NowPlaying url='https://movie-db-omega-ten.vercel.app/movies/nowPlaying' mediaType='movie' /> } />
-          <Route path='/nowPlaying/tv' element={ <NowPlaying url='https://movie-db-omega-ten.vercel.app/tv/onTheAir' mediaType='tv' /> } />
-
-          <Route path='/topRated/movies' element={ <TopRated url='https://movie-db-omega-ten.vercel.app/movies/topRated' mediaType='movie' /> } />
-          <Route path='/topRated/tv' element={ <TopRated url='https://movie-db-omega-ten.vercel.app/tv/topRated' mediaType='tv' /> } />
-          
+          <Route path='/popular/:type' element={ <Popular /> } />
+          <Route path='/nowPlaying/:type' element={ <NowPlaying /> } />
+          <Route path='/topRated/:type' element={ <TopRated /> } />
           <Route path='/upcoming/movies' element={ <Upcoming /> } />
 
           <Route path='*' element={<NoPage />} />
         </Routes>
+
         <Footer /> 
       </BrowserRouter>
     </div>
