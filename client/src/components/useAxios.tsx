ead2dccdbbab10d131dcc3e708413863215e2e1a
ScottHahn1 <scotthahn2210@gmail.com>
2025-10-24 12:04:37 +0200

    refactor: add sendCredentials parameter to useAxios and remove headers parameter

--- client/src/components/useAxios.tsx
+++ client/src/components/useAxios.tsx
@@ -1,19 +1,22 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const useAxios = <S, T>(url: string, params: T, headers?: Record<string, string>) => {
+const useAxios = <S, T>(url: string, params: T, sendCredentials: boolean = false) => {
     const [data, setData] = useState<S | undefined>(undefined);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
-
-    axios.defaults.withCredentials = true;
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 setLoading(true);
                 setError(false);
-                const res = await axios({ method: 'GET', url, headers, params });
+                const res = await axios({ 
+                    method: 'GET', 
+                    url, 
+                    params,
+                    withCredentials: sendCredentials 
+                });
                 setData(res.data);
             } catch {
                 setError(true);
@@ -24,7 +27,7 @@ const useAxios = <S, T>(url: string, params: T, headers?: Record<string, string>
 
         fetchData();
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [url, JSON.stringify(params), headers])
+    }, [url, JSON.stringify(params), sendCredentials])
 
     return { data, loading, error };
 }
