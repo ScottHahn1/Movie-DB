f7b1764b4c9aaeb0869de8e440dc835e900da788
ScottHahn1 <scotthahn2210@gmail.com>
2025-11-05 18:53:56 +0200

    feat: add utility functions for handling user favourites (add, delete, click handler)

---
+++ client/src/utils/favourites.ts
@@ -0,0 +1,107 @@
+import axios from "axios";
+import { API_URL } from "../config/api";
+import { User } from "../hooks/useAuth";
+import { Dispatch, SetStateAction } from "react";
+import { DetailsType } from "../typeAliases/Details";
+
+const addMovieToFavourites = async (
+    user: User,
+    setIsFavourited: Dispatch<SetStateAction<boolean>>,
+    mediaId: number,
+    title: string,
+    releaseDate: string,
+    posterPath: string,
+    voteAverage: number,
+    overview: string,
+    mediaType: string,
+    runtime?: number,
+) => {
+    await axios.post(
+        `${API_URL}/favourites/add`,
+        {
+            userId: user?.userId,
+            mediaId: mediaId,
+            title: title,
+            releaseDate: releaseDate,
+            posterPath: posterPath,
+            overview: overview,
+            voteAverage: voteAverage,
+            runtime: runtime,
+            mediaType: mediaType,
+        },
+        {
+            withCredentials: true,
+        }
+    );
+    setIsFavourited(true);
+};
+
+const deleteMovie = async (
+    userId: number, 
+    id: number, 
+    setIsFavourited: Dispatch<SetStateAction<boolean>>
+) => {
+    try {
+        await axios.delete(`${API_URL}/favourites/delete/${id}`, { 
+            params: { userId: userId },
+            withCredentials: true
+        });
+        setIsFavourited(false);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const handleFavouriteClick = (
+    user: User | null, 
+    isFavourited: boolean, 
+    setIsFavourited: Dispatch<SetStateAction<boolean>>,
+    details: DetailsType,
+    mediaType: string | undefined,
+    setFavouritedError: Dispatch<SetStateAction<boolean>>,
+    setShowRatingsBar: Dispatch<SetStateAction<boolean>>
+) => {
+    if (user && mediaType) {
+        if (isFavourited) {
+            if (details) {
+                deleteMovie(user.userId, details.id, setIsFavourited);
+            }
+        } else {
+            if (mediaType === 'movie') {
+                if (details) {
+                    addMovieToFavourites(
+                        user,
+                        setIsFavourited,
+                        details.id, 
+                        details.title, 
+                        details.release_date, 
+                        details.poster_path, 
+                        details.vote_average, 
+                        details.overview, 
+                        mediaType,
+                        details.runtime
+                    ) 
+                }
+            } else {
+                if (details) {
+                    addMovieToFavourites(
+                        user,
+                        setIsFavourited,
+                        details.id, 
+                        details.name, 
+                        details.first_air_date, 
+                        details.poster_path, 
+                        details.vote_average, 
+                        details.overview,
+                        mediaType,
+                    )
+                }
+            }
+        }
+    } else {
+        setFavouritedError(true);
+        setShowRatingsBar(false)
+    }
+}
+
+export { addMovieToFavourites, deleteMovie, handleFavouriteClick };
