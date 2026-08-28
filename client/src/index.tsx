11ed9e828a69f2324d123ce0d2d5644f6c20b4b2
ScottHahn1 <scotthahn2210@gmail.com>
2026-08-28 11:22:36 +0200

    refactor: remove unused index.css

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
 import App from './App';
-import './assets/fonts/Montserrat-Black.ttf';
-import './assets/fonts/Poppins-Bold.ttf';
-import './assets/fonts/Roboto-Regular.ttf';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
